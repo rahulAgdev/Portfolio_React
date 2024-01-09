cf92cc1748509bccd780cb1a9a9e0ee3f11faa89
Rahul Agarwal <rahulagdev@gmail.com>
2024-01-10 02:08:38 +0530

    Adding higher order wrapping component

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,44 +2,44 @@ import React, { useState, useEffect } from "react";
 import "./About.scss";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
-import { urlFor, client } from "../../client";
-// const abouts = [
-//   {
-//     title: "Web Development",
-//     descriptions: "I am a good Web Developer.",
-//     imgURL: images.about01,
-//   },
-//   {
-//     title: "Web Design",
-//     descriptions: "I am a good Web Developer.",
-//     imgURL: images.about02,
-//   },
-//   {
-//     title: "UI/UX",
-//     descriptions: "I am a good Web Developer.",
-//     imgURL: images.about03,
-//   },
-//   {
-//     title: "React",
-//     descriptions: "I am a good Web Developer.",
-//     imgURL: images.about04,
-//   },
-// ];
+import {AppWrap} from '../../wrapper'
+// import { urlFor, client } from "../../client";
+const abouts = [
+  {
+    title: "Web Development",
+    descriptions: "I am a good Web Developer.",
+    imgURL: images.about01,
+  },
+  {
+    title: "Web Design",
+    descriptions: "I am a good Web Developer.",
+    imgURL: images.about02,
+  },
+  {
+    title: "UI/UX",
+    descriptions: "I am a good Web Developer.",
+    imgURL: images.about03,
+  },
+  {
+    title: "React",
+    descriptions: "I am a good Web Developer.",
+    imgURL: images.about04,
+  },
+];
 
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
-    client.fetch(query).then((data) => {
-      setAbouts(data);
-    });
-  }, []);
+//   const [abouts, setAbouts] = useState([]);
+//   useEffect(() => {
+//     const query = '*[_type == "abouts"]';
+//     client.fetch(query).then((data) => {
+//       setAbouts(data);
+//     });
+//   }, []);
 
 return (
     <>
-   
     
-      <h2 id="about" className="head-text">
+      <h2 className="head-text">
         I know That<span> Good Design</span>
         <br />
         means<span> Good Business</span>
@@ -54,6 +54,7 @@ return (
             className="app__profile-item"
             key={about.title + index}
           >
+            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
             <img src={about.imgURL} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
@@ -68,4 +69,4 @@ return (
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
