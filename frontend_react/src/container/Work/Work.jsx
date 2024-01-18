e795386dcca97d2ab0feeb4ff07fd4b276ec5156
Rahul Agarwal <rahulagdev@gmail.com>
2024-01-19 00:12:09 +0530

    Adding work section elements

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -62,12 +62,40 @@ const Work = () => {
               transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
               className="app__work-hover app__flex"
             >
-              <a href={work.projetLink} target="_blank" >
+              <a href={work.projectLink} target="_blank" rel="noreferrer">
+
+              <motion.div
+              whileInView={{scale: [0,1]}}
+                whileHover={{scale: [1,0.9]}}
+              transition={{duration: 0.25}}
+              className=" app__flex"
+              >
+                <AiFillEye />
+              </motion.div>
+                
+              </a>
+              <a href={work.codeLink} target="_blank" rel="noreferrer">
+
+              <motion.div
+              whileInView={{scale: [0,1]}}
+                whileHover={{scale: [1,0.9]}}
+              transition={{duration: 0.25}}
+              className=" app__flex"
+              >
+                <AiFillGithub />
+              </motion.div>
                 
               </a>
             </motion.div>
           </div>
+          <div className="app__work-content app__flex">
+            <h4 className="bold-text">{work.title}</h4>
+            <p className="p-text" style={{marginTop: 10}}>{work.description}</p>
 
+            <div className="app__work-tag app__flex">
+              <p className="p-text">{work.tags[0]}</p>
+            </div>
+          </div>
           </div>
         ))}
       </motion.div>
@@ -75,4 +103,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default AppWrap(Work, 'work');
