99483e5aacf2457609c100381fe9bd94b51e60cc
Rahul Agarwal <rahulagdev@gmail.com>
2024-01-18 16:41:23 +0530

    Adding client server connection to the work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -9,6 +9,21 @@ import { urlFor, client } from "../../client";
 const Work = () => {
   const handleWorkFilter = (item) => {};
   const [activeFilter, setActiveFilter] = useState('All')
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
+  const [works, setWorks] = useState([])
+  const [FilterWork, setFilterWork] = useState([])
+
+  useEffect(() => {
+    
+    const query = '*[_type=="works"]';
+    client.fetch(query)
+      .then((data)=>{
+        setWorks(data);
+        setFilterWork(data);
+      })
+    
+  }, [])
+  
   return (
     <>
       <h2 className="head-text">
@@ -28,6 +43,34 @@ const Work = () => {
           </div>
         ))}
       </div>
+      <motion.div
+        animate = {animateCard}
+        transition={{duration: 0.5, delayChildren: 0.5}}
+        className="app__work-portfolio"
+
+      >
+        {FilterWork.map((work,index)=>(
+          <div className="app__work-item app__flex" key={index}>
+
+          <div className="app__work-img app__flex ">
+            <img 
+              src={urlFor(work.imgUrl)}
+              alt={work.name}
+            />
+            <motion.div
+              whileHover={{opacity: [0,1]}}
+              transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+              className="app__work-hover app__flex"
+            >
+              <a href={work.projetLink} target="_blank" >
+                
+              </a>
+            </motion.div>
+          </div>
+
+          </div>
+        ))}
+      </motion.div>
     </>
   );
 };
